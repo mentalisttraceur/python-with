13b8be3a1e5e8656eb8144854b9b81bcf65c9608
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2020-10-09 06:35:07 +0000

    Remove docstring note about traceback variant

--- with_no_traceback.py
+++ with_no_traceback.py
@@ -16,12 +16,6 @@
 
 Provides a minimal, clean and portable interface for using context
 managers with all the advantages of functions over syntax.
-
-Note:
-    This is the "no traceback" variant, meant for Python implementations
-    that do not support getting a traceback object for an exception. It
-    exists to allow code using the ``with_`` interface to gracefully
-    degrade in the absence of full traceback support.
 """
 
 __all__ = ('with_',)
@@ -50,12 +44,6 @@ def with_(manager, action):
         Any: If raised by calling the action and not suppressed by the
             manager, or if raised by the manager, or if the manager
             does not implement the context manager protocol correctly.
-
-    Note:
-        This "no traceback" variant, meant for Python implementations
-        that do not support getting a traceback object for an exception,
-        always passes ``None`` for the traceback argument to the context
-        manager's ``__exit__``.
     """
     exit = type(manager).__exit__
     value = type(manager).__enter__(manager)
