d460cc3ff979348e84ed24f4c803f74e5fc093de
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2020-10-14 00:55:53 +0000

    Package the variants for different versions

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 default:
-	python setup.py sdist bdist_wheel --universal
+	python setup.py sdist
+	python setup.py bdist_wheel --python-tag py20
+	rm build/lib/with_.py
+	python setup.py bdist_wheel --python-tag py25
+	rm build/lib/with_.py
+	python setup.py bdist_wheel --python-tag py26.py3
 
 clean:
 	rm -rf __pycache__ *.py[oc] build *.egg-info dist
--- setup.py
+++ setup.py
@@ -1,22 +1,63 @@
 #!/usr/bin/env python
 
-from os import path
+import errno
+import os
+import sys
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-from with_ import __doc__, __version__
+from normal import __doc__, __version__
 
-project_directory = path.abspath(path.dirname(__file__))
-readme_path = path.join(project_directory, 'README.rst')
+project_directory = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(project_directory, 'README.rst')
 
 readme_file = open(readme_path)
 try:
     long_description = readme_file.read()
 finally:
     readme_file.close()
+
+
+def bdist_wheel_tag_check(tag, args=sys.argv):
+    if 'bdist_wheel' not in args:
+        return False
+    if '--python-tag' in args and tag in args:
+        return True
+    if ('--python-tag=' + tag) in args:
+        return True
+    return False
+
+
+if 'sdist' in sys.argv:
+    # When building a source distribution, include all variants:
+    modules = ['normal', 'from_future_import', 'manual', 'manual_no_traceback']
+else:
+    # When not building a source distribution, we select
+    # just the file for the matching Python version:
+    modules = ['with_']
+
+    path_setup_py_uses = os.path.join(project_directory, 'with_.py')
+    if bdist_wheel_tag_check('py26.py3'):
+        source_path = os.path.join(project_directory, 'normal.py')
+    elif bdist_wheel_tag_check('py25'):
+        source_path = os.path.join(project_directory, 'from_future_import.py')
+    elif bdist_wheel_tag_check('py20'):
+        source_path = os.path.join(project_directory, 'manual.py')
+    elif sys.version_info >= (2, 6):
+        source_path = os.path.join(project_directory, 'normal.py')
+    elif sys.version_info >= (2, 5):
+        source_path = os.path.join(project_directory, 'from_future_import.py')
+    else:
+        source_path = os.path.join(project_directory, 'manual.py')
+    try:
+        os.unlink(path_setup_py_uses)
+    except OSError as error:
+        if error.errno != errno.ENOENT:
+            raise
+    os.link(source_path, path_setup_py_uses)
 
 setup(
     name='with-as-a-function',
@@ -33,5 +74,5 @@ setup(
         'Programming Language :: Python :: 2',
         'Operating System :: OS Independent',
     ],
-    py_modules=['with_'],
+    py_modules=modules,
 )
