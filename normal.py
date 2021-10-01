7265c2c3f9d583bb0de7d48343ade806bdacbf9d
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2021-10-01 06:18:51 +0000

    Shorten license comment to SPDX identifier

--- normal.py
+++ normal.py
@@ -1,16 +1,5 @@
+# SPDX-License-Identifier: 0BSD
 # Copyright 2019 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
-#
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 
 """Use context managers with a function instead of a statement.
 
@@ -19,7 +8,7 @@ managers with all the advantages of functions over syntax.
 """
 
 __all__ = ('with_',)
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 
 def with_(manager, action):
