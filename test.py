6675c2df972d57d500a6904255bb954864705bc9
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2022-12-10 23:14:25 +0000

    test scraps 2
    Testing so far remains pretty manual, partly because
    setting up automated testing that works across all
    of the supported Pythons is its own portability
    challenge. But this is another move in the direction
    of proper automated tests once day.
    
    Another move is getting to a point where I can run
    all of the browser Pythons in the same environment.
    
    * Skulpt:
      1. `npm run build` possibly with the environment variable
         `NODE_OPTIONS=--openssl-legacy-provider`, then
      2. `npm run repl (py2|py3)`, but still need to figure out
         how to reliably feed Python into this, or alternatively
         how to run things without going through that REPL.
    * Transcrypt:
      1. build with `transcrypt` (`-n` option avoids the Java
         minifier dependency, and `-b` to rebuild everything
         each time might be useful.), and
      2. use modules in __target__ with Node (or maybe Deno?),
         possibly after adjusting imports + renaming .js->.mjs.
    * Brython: only briefly looked into this one, not sure if
      there is any official or polished way to run it locally.
    
    For contrast, Pyodide just uses a browser through Selenium
    for tests, and maybe that's the way to go.

--- test.py
+++ test.py
@@ -1,7 +1,29 @@
 import sys
 
 from raise_ import raise_
-import manual
+
+
+_with_list = []
+_iwith_list = []
+
+
+def _import(with_module_name):
+    module = __import__(with_module_name)
+    _with_list.append(module.with_)
+    _iwith_list.append(module.iwith)
+
+if sys.version_info >= (3, 3):
+    _import('with_3_3')
+    _import('with_brython')
+    _import('with_skulpt_3')
+else:
+    _import('with_2_6')
+    _import('with_2_5')
+    _import('with_2_2')
+    _import('with_skulpt_2')
+    if sys.version_info >= (3, 0):
+        _import('with_skulpt_3')
+print([with_.__module__ for with_ in _with_list])
 
 
 class ContextManager(object):
@@ -18,19 +40,26 @@ class ContextManager(object):
         return self.suppress
 
 
-class ObjectWithAttributes(object): pass
+class ObjectWithAttributes(object):
+    pass
 
 
-def _test_normal(with_):
+def _test(test_function, with_list):
+    for with_ in with_list:
+        print(test_function.__name__ + ' ' + with_.__module__)
+        test_function(with_)
+
+
+def _test_return(with_):
     nonce = ObjectWithAttributes()
     result = with_(ContextManager(False, nonce), lambda x: x)
-    assert nonce is result
+    assert result is nonce
     assert nonce.entered == 'entered'
     assert nonce.exited == 'exited'
 
 
-def test_normal():
-    _test_normal(manual.with_)
+def test_return():
+    _test(_test_return, _with_list)
 
 
 def _test_raise(with_):
@@ -45,7 +74,7 @@ def _test_raise(with_):
 
 
 def test_raise():
-    _test_raise(manual.with_)
+    _test(_test_raise, _with_list)
 
 
 def _test_suppress(with_):
@@ -57,10 +86,105 @@ def _test_suppress(with_):
 
 
 def test_suppress():
-    _test_suppress(manual.with_)
+    _test(_test_suppress, _with_list)
+
+
+def _test_iterate(iwith):
+    nonce = ObjectWithAttributes()
+    def generator(x):
+        yield x
+        yield x
+        yield x
+    iterator = iwith(ContextManager(False, nonce), generator)
+    result = list(iterator)
+    assert len(result) == 3
+    assert result[0] is nonce
+    assert result[1] is nonce
+    assert result[2] is nonce
+    assert nonce.entered == 'entered'
+    assert nonce.exited == 'exited'
+
+
+def test_iterate():
+    _test(_test_iterate, _iwith_list)
+
+
+def _test_iterate_empty(iwith):
+    nonce = ObjectWithAttributes()
+    iterator = iwith(ContextManager(False, nonce), lambda x: ())
+    result = list(iterator)
+    assert result == []
+    assert nonce.entered == 'entered'
+    assert nonce.exited == 'exited'
+
+
+def test_iterate_empty():
+    _test(_test_iterate_empty, _iwith_list)
+
+
+def _test_send(iwith):
+    nonce = ObjectWithAttributes()
+    def generator(x):
+        yield (yield x)
+    iterator = iwith(ContextManager(False, nonce), generator)
+    first = next(iterator)
+    second = iterator.send(42)
+    rest = list(iterator)
+    assert first is nonce
+    assert second == 42
+    assert rest == []
+    assert nonce.entered == 'entered'
+    assert nonce.exited == 'exited'
+
+
+def test_send():
+    _test(_test_send, _iwith_list)
+
+
+def _test_send_prematurely(iwith):
+    nonce = ObjectWithAttributes()
+    def generator(x):
+        yield x
+    iterator = iwith(ContextManager(False, nonce), generator)
+    try:
+        iterator.send(57)
+    except TypeError:
+        pass
+    else:
+        assert False, 'iterator.send() should have raised'
+    result = list(iterator)
+    assert result == [nonce]
+    assert nonce.entered == 'entered'
+    assert nonce.exited == 'exited'
+
+
+def test_send_prematurely():
+    _test(_test_send_prematurely, _iwith_list)
+
+
+def _test_send_none_prematurely(iwith):
+    nonce = ObjectWithAttributes()
+    def generator(x):
+        yield x
+    iterator = iwith(ContextManager(False, nonce), generator)
+    result = iterator.send(None)
+    rest = list(iterator)
+    assert result is nonce
+    assert rest == []
+    assert nonce.entered == 'entered'
+    assert nonce.exited == 'exited'
+
+
+def test_send_none_prematurely():
+    _test(_test_send_none_prematurely, _iwith_list)
 
 
 if __name__ == '__main__':
-    test_normal()
+    test_return()
     test_raise()
     test_suppress()
+    test_iterate()
+    test_iterate_empty()
+    test_send()
+    test_send_prematurely()
+    test_send_none_prematurely()
