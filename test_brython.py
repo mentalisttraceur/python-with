b87a2f0a178851a566126f24c494afed7c4880d6
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-01-20 17:26:16 +0000

    test scraps 3
    Brython tests can run in Node.js with `scripts/node_bridge.py`.

--- test_brython.py
+++ test_brython.py
@@ -1,27 +1,70 @@
-import sys
+# SPDX-License-Identifier: 0BSD
+# Copyright 2019 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
+
+"""Use context managers with a function instead of a statement.
+
+Provides a minimal and portable interface for using context
+managers with all the advantages of functions over syntax.
+"""
+
+__all__ = ('with_', 'iwith')
+__version__ = '1.1.0'
 
 
-_with_list = []
-_iwith_list = []
+def with_(manager, action):
+    """Execute an action within the scope of a context manager.
+
+    Arguments:
+        manager: The context manager instance to use.
+        action: The callable to execute. Must accept the ``as`` value
+            of the context manager as the only positional argument.
+
+    Returns:
+        Any: Return value of the executed action.
+        None: If the manager suppresses an exception from the action.
+
+    Raises:
+        Any: If raised by calling the action and not suppressed by the
+            manager, or if raised by the manager, or if the manager
+            does not implement the context manager protocol correctly.
+    """
+    with manager as value:
+        return action(value)
+    return None
 
 
-def _import(with_module_name):
-    module = __import__(with_module_name)
-    _with_list.append(module.with_)
-    _iwith_list.append(module.iwith)
+def iwith(manager, action):
+    """Iterate within the scope of a context manager.
 
-if sys.version_info >= (3, 3):
-    _import('with_3_3')
-    _import('with_brython')
-    _import('with_skulpt_3')
-else:
-    _import('with_2_6')
-    _import('with_2_5')
-    _import('with_2_2')
-    _import('with_skulpt_2')
-    if sys.version_info >= (3, 0):
-        _import('with_skulpt_3')
-print([with_.__module__ for with_ in _with_list])
+    Arguments:
+        manager: The context manager instance to use.
+        action: The callable to execute to get an iterator. Must
+            accept the ``as`` value of the context manager as
+            the only positional argument and return an iterable.
+
+    Yields:
+        Any: Values from the iterable returned by the action.
+
+    Returns:
+        Any: The value "returned" inside the StopIteration exception
+            that is raised once the iterable's iterator is exhausted.
+        None: If the manager suppresses an exception from the iteration.
+
+    Raises:
+        Any: If raised by calling the action or while iterating and
+            not suppressed by the manager, or if raised by the manager,
+            or if the manager does not implement the context manager
+            protocol correctly, or if raised while yielding values
+            from and delegating to the iterable returned by the action.
+    """
+    with manager as value:
+        return (yield from action(value))
+    return None
+
+
+__name__ = '__main__'
+_with_list = [with_]
+_iwith_list = [iwith]
 
 
 try:
@@ -62,14 +105,6 @@ class _Iterator(object):
 class _Closable(_Iterator):
     def close(self):
         self.thing.closed = 'closed'
-
-
-class _Sendable(_Iterator):
-    def send(self, value):
-        if not hasattr(self.thing, 'sent'):
-            self.thing.sent = []
-        self.thing.sent.append(value)
-        return thing
 
 
 class ObjectWithAttributes(object):
